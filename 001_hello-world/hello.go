1a7a5859efd538d1886e933983a92e1a635ee3f0
manikandanelangovan <manoelangoo@gmail.com>
2019-11-03 12:21:09 +0530

    Inital Commit Hello-World

---
+++ 001_hello-world/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// [go fmt] command
+/*
+	Used to format code via command line
+*/
+// [go fmt ./..] command
+/*
+	To format all the '.go' files in the directory
+*/
+func main() {
+	fmt.Println("Usual convention to have this program while learning new lang")
+	fmt.Println("Hello World :-) ")
+}
